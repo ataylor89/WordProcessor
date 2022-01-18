78537a9d4e7244475aca3f08f439120e91eeaa82
Andrew Taylor <andrewtaylor@Andrews-MacBook-Pro.local>
2022-01-17 22:59:51 -0600

    Adding bookspenink.png icon Updated macbuild.sh to create icons and to compile and build the Java code

--- macbuild.sh
+++ macbuild.sh
@@ -1,3 +1,20 @@
+if [ ! -f icons.icns ]; then
+    ICON="icons/bookspenink.png"   
+    mkdir icons.iconset
+    sips -z 512 512   $ICON --out icons.iconset/icon_512x512.png
+    cp $ICON icons.iconset/icon_512x512@2x.png
+    sips -z 512 512   $ICON --out icons.iconset/icon_256x256@2x.png
+    sips -z 256 256   $ICON --out icons.iconset/icon_256x256.png
+    sips -z 256 256   $ICON --out icons.iconset/icon_128x128@2x.png
+    sips -z 128 128   $ICON --out icons.iconset/icon_128x128.png
+    sips -z 64 64     $ICON --out icons.iconset/icon_32x32@2x.png
+    sips -z 32 32     $ICON --out icons.iconset/icon_32x32.png
+    sips -z 32 32     $ICON --out icons.iconset/icon_16x16@2x.png
+    sips -z 16 16     $ICON --out icons.iconset/icon_16x16.png
+    iconutil -c icns icons.iconset
+fi
+mvn clean install
+mvn assembly:single
 jpackage --input target/ \
   --name WordProcessor \
   --main-jar WordProcessor-jar-with-dependencies.jar \
